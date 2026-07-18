1e52b33788d223f7bacb4d3ccd50125cc8341f36
Jesus <sisniega.serrano@gmail.com>
2026-07-18 10:05:38 -0400

    Recursion p.206

---
+++ Easy/Reverse_Linked_List.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (head == nullptr) {
+            return head;
+        }
+        else {
+            ListNode* rev = nullptr;
+
+            while (head != nullptr) {
+                ListNode* curr = head;
+                head = head->next;
+                curr->next = rev;
+                rev = curr;
+            }
+
+            return rev;
+        }
+    }
+};
